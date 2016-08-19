c584ded4a6acf0805e358703d27113dee23f8850
Guilherme Bayer <cin_gbayer@uolinc.com>
2016-08-19 15:48:52 -0300

    remove gulp from project

--- src/scripts/selectshare.js
+++ src/scripts/selectshare.js
@@ -1,20 +1,20 @@
-var SelectShr = (function() {
-  var $public  = {},
-      $private = {};
+const selectShare = () => {
+  const $public  = {}
+  const $private = {}
 
-  $private.init = function() {
-    document.addEventListener('mouseup', function(el) {
-      var x             = el.clientX;
-      var y             = el.clientY;
-      var selectionText = getSelectionText().length;
-      var selectShr     = document.querySelector('.selectShr');
+  $private.init = () => {
+    document.addEventListener('mouseup', el => {
+      let x = el.clientX
+      let y = el.clientY
+      let selectionText = getSelectionText().length
+      let selectShr = document.querySelector('.selectshare')
 
       selectShrPosition(x, y, selectShr);
 
       if(selectionText > 0)
-        selectShr.classList.add('selectShrOpen');
+        selectShr.classList.add('selectshare-open');
       else
-        selectShr.classList.remove('selectShrOpen');
+        selectShr.classList.remove('selectshare-open');
 
       console.log(getSelectionText());
     });
@@ -25,32 +25,32 @@ var SelectShr = (function() {
     };
 
     function selectShrPosition(selectShrX, selectShrY, selectShr) {
-      selectShr.style.position = 'absolute';
-      selectShr.style.left     = (selectShrX - 40) + 'px';
-      selectShr.style.top      = (selectShrY - 55) + 'px';
+      selectShr.style.position = 'absolute'
+      selectShr.style.left = (selectShrX - 40) + 'px'
+      selectShr.style.top = (selectShrY - 55) + 'px'
     };
-  };
+  }
 
   $public.init = () => {
-    var selectShrContainer = document.createElement('div');
-    var facebook = document.createElement('a');
-    var twitter  = document.createElement('a');
-    var gplus    = document.createElement('a');
+    let selectShrContainer = document.createElement('div')
+    let facebook = document.createElement('a')
+    let twitter  = document.createElement('a')
+    let gplus    = document.createElement('a')
 
-    facebook.classList.add('flaticon-circle');
-    twitter.classList.add('flaticon-social');
-    gplus.classList.add('flaticon-social-media');
+    facebook.classList.add('flaticon-circle')
+    twitter.classList.add('flaticon-social')
+    gplus.classList.add('flaticon-social-media')
 
-    selectShrContainer.appendChild(facebook);
-    selectShrContainer.appendChild(twitter);
-    selectShrContainer.appendChild(gplus);
+    selectShrContainer.appendChild(facebook)
+    selectShrContainer.appendChild(twitter)
+    selectShrContainer.appendChild(gplus)
 
-    selectShrContainer.classList.add('selectShr');
-    document.body.appendChild(selectShrContainer);
-    // selectShrContainer.appendChild(facebook);
+    selectShrContainer.classList.add('selectShr')
+    document.body.appendChild(selectShrContainer)
+    // selectShrContainer.appendChild(facebook)
 
-    return $private.init();
-  };
+    return $private.init()
+  }
 
-  return $public.init();
-});
+  return $public.init()
+}
