cab19531356a11fbb3c72a52111f0734e6048401
Jeff Lalic <jefflalic@gmail.com>
2016-10-02 11:38:11 -0700

    Add lint-standard gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp'),
     babel = require('gulp-babel'),
-    cleanCSS = require('gulp-clean-css');
+    cleanCSS = require('gulp-clean-css'),
+    standard = require('gulp-standard')
 
 gulp.task('babel', () => {
      gulp.src('src/scripts/**.js')
@@ -12,12 +13,21 @@ gulp.task('babel', () => {
 
 gulp.task('minify-css', () => {
     gulp.src('src/styles/**.css')
-      .pipe(cleanCSS({ processImport: false }))
-      .pipe(gulp.dest('dist/styles'));
+        .pipe(cleanCSS({ processImport: false }))
+        .pipe(gulp.dest('dist/styles'));
 });
+
+gulp.task('lint-standard', () => {
+    gulp.src(['src/scripts/**.js'])
+        .pipe(standard())
+        .pipe(standard.reporter('default', {
+            breakOnError: true,
+            quiet: true
+        }))
+})
 
 gulp.task('watch', () => {
     gulp.watch(['src/scripts/**.js'], ['babel'])
 })
 
-gulp.task('build', ['babel'])
+gulp.task('build', ['lint-standard', 'babel'])
