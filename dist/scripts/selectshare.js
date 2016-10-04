5748a791d78c5f1204547fa10b3ae302da4c390e
guuibayer <guiilherme.bayer@gmail.com>
2016-10-04 19:07:47 -0300

    refactoring in structure

--- dist/scripts/selectshare.js
+++ dist/scripts/selectshare.js
@@ -1,47 +1,51 @@
 'use strict';
 
 var SelectShare = function SelectShare() {
-    document.addEventListener('mouseup', function (element) {
-        var x = element.clientX;
-        var y = element.clientY;
-        var selectShareText = getSelementectionText().length;
-        var selectShareElement = document.querySelector('.select-share');
-        var selectShareClass = selectShare.classList;
+    this.eventListener = function () {
+        document.addEventListener('mouseup', function (element) {
+            var x = element.clientX;
+            var y = element.clientY;
+            var selectShareText = getSelementectionText().length;
+            var selectShareElement = document.querySelector('.select-share');
+            var selectShareClass = selectShare.classList;
 
-        selectSharePosition(x, y, selectShare);
+            selectSharePosition(x, y, selectShare);
 
-        if (!selectionText) {
-            selectShareClass.remove('selectshare-open');
-        }
+            if (!selectionText) {
+                selectShareClass.remove('selectshare-open');
+            }
 
-        return selectShareClass.add('selectshare-open');
-    });
+            return selectShareClass.add('selectshare-open');
+        });
+    };
 
-    function getSelectionText() {
+    this.getSelectedText = function () {
         return window.getSelection().toString();
-    }
+    };
 
-    function selectSharePosition(selectShareX, selectShareY, selectShare) {
+    this.getSharePosition = function (selectShareX, selectShareY, selectShare) {
         var selectShareStyle = selectShare.style;
 
         selectShareStyle.position = 'absolute';
         selectShareStyle.left = selectShareX + 'px';
         selectShareStyle.top = selectShareY + 'px';
-    }
+    };
+
+    this.createShareTooltip = function () {
+        var selectShareContainer = document.createElement('div');
+        var facebook = document.createElement('a');
+        var twitter = document.createElement('a');
+        var googlePlus = document.createElement('a');
+
+        facebook.classList.add('flaticon-circle');
+        twitter.classList.add('flaticon-social');
+        googlePlus.classList.add('flaticon-social-media');
+
+        selectShareContainer.appendChild(facebook);
+        selectShareContainer.appendChild(twitter);
+        selectShareContainer.appendChild(gplus);
+
+        selectShareContainer.classList.add('select-share');
+        document.body.appendChild(selectShareContainer);
+    };
 };
-
-var selectShareContainer = document.createElement('div');
-var facebook = document.createElement('a');
-var twitter = document.createElement('a');
-var googlePlus = document.createElement('a');
-
-facebook.classList.add('flaticon-circle');
-twitter.classList.add('flaticon-social');
-googlePlus.classList.add('flaticon-social-media');
-
-selectShareContainer.appendChild(facebook);
-selectShareContainer.appendChild(twitter);
-selectShareContainer.appendChild(gplus);
-
-selectShareContainer.classList.add('select-share');
-document.body.appendChild(selectShareContainer);
--- src/scripts/selectshare.js
+++ src/scripts/selectshare.js
@@ -1,4 +1,5 @@
-    const SelectShare = () => {
+var SelectShare = function() {
+    this.eventListener = () => {
         document.addEventListener('mouseup', element => {
             let x = element.clientX
             let y = element.clientY
@@ -8,38 +9,43 @@
 
             selectSharePosition(x, y, selectShare)
 
-        if(!selectionText) {
-            selectShareClass.remove('selectshare-open')
-        }
+            if(!selectionText) {
+                selectShareClass.remove('selectshare-open')
+            }
 
-        return selectShareClass.add('selectshare-open')
-      })
-
-        function getSelectionText() {
-            return window.getSelection().toString()
-        }
-
-        function selectSharePosition(selectShareX, selectShareY, selectShare) {
-            let selectShareStyle = selectShare.style
-
-            selectShareStyle.position = 'absolute'
-            selectShareStyle.left = `${selectShareX}px`
-            selectShareStyle.top = `${selectShareY}px`
-        }
+            return selectShareClass.add('selectshare-open')
+        })
     }
 
-    let selectShareContainer = document.createElement('div')
-    let facebook = document.createElement('a')
-    let twitter = document.createElement('a')
-    let googlePlus = document.createElement('a')
+    this.getSelectedText = () => {
+        return window
+                    .getSelection()
+                    .toString()
+    }
 
-    facebook.classList.add('flaticon-circle')
-    twitter.classList.add('flaticon-social')
-    googlePlus.classList.add('flaticon-social-media')
+    this.getSharePosition = (selectShareX, selectShareY, selectShare) => {
+        let selectShareStyle = selectShare.style
 
-    selectShareContainer.appendChild(facebook)
-    selectShareContainer.appendChild(twitter)
-    selectShareContainer.appendChild(gplus)
+        selectShareStyle.position = 'absolute'
+        selectShareStyle.left = `${selectShareX}px`
+        selectShareStyle.top = `${selectShareY}px`
+    }
 
-    selectShareContainer.classList.add('select-share')
-    document.body.appendChild(selectShareContainer)
+    this.createShareTooltip = () => {
+        let selectShareContainer = document.createElement('div')
+        let facebook = document.createElement('a')
+        let twitter = document.createElement('a')
+        let googlePlus = document.createElement('a')
+
+        facebook.classList.add('flaticon-circle')
+        twitter.classList.add('flaticon-social')
+        googlePlus.classList.add('flaticon-social-media')
+
+        selectShareContainer.appendChild(facebook)
+        selectShareContainer.appendChild(twitter)
+        selectShareContainer.appendChild(gplus)
+
+        selectShareContainer.classList.add('select-share')
+        document.body.appendChild(selectShareContainer)
+    }
+}
