67c7e8e5bc7aebde191d71f22e7f17379eb98afb
guuibayer <guiilherme.bayer@gmail.com>
2017-01-08 12:26:32 -0200

    Fix in gulpfile, icons and refactoring methods from Selectshare class

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,53 @@
 const gulp = require('gulp')
       , babel = require('gulp-babel')
       , uglify = require('gulp-uglify')
-      , cleanCSS = require('gulp-clean-css')
-      , standard = require('gulp-standard');
+      , fontmin = require('gulp-fontmin')
+      , rename = require('gulp-rename')
+      , cleancss = require('gulp-clean-css');
 
-gulp.task('babel', () => {
-  gulp.src('src/scripts/**.js')
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .pipe(gulp.dest('dist/scripts/'));
-})
 
-gulp.task('minify-js', () => {
-  gulp.src('./dist/scripts/selectshare.js')
+let path = {
+  src: {
+    scripts: "src/scripts/selectshare.js",
+    styles: "src/styles/selectshare.css",
+    fonts: "src/fonts/*"
+  },
+  dist: {
+    scripts: "dist/scripts",
+    styles: "dist/styles",
+    fonts: "dist/fonts"
+  }
+}
+
+
+gulp.task('fonts', () => {
+  gulp.src(path.src.fonts)
+    .pipe(fontmin())
+    .pipe(gulp.dest(path.dist.fonts));
+});
+
+
+gulp.task('scripts', () => {
+  gulp.src(path.src.scripts)
+    .pipe(babel({ presets: ['es2015'] }))
     .pipe(uglify())
     .pipe(rename('selectshare.min.js'))
-    .pipe(gulp.dest('./dist/scripts'));
+    .pipe(gulp.dest(path.dist.scripts));
 });
 
-gulp.task('minify-css', () => {
-    gulp.src('src/styles/**.css')
-      .pipe(cleanCSS({ processImport: false }))
-      .pipe(gulp.dest('dist/styles/'));
+
+gulp.task('styles', () => {
+  gulp.src(path.src.styles)
+    .pipe(cleancss({ processImport: false }))
+    .pipe(gulp.dest(path.dist.styles));
 });
+
 
 gulp.task('default', () => {
-    gulp.watch(['src/scripts/**.js'], ['babel']);
-    gulp.watch(['src/styles/**.css'], ['minify-css']);
+  gulp.watch(path.src.scripts, ['scripts']);
+  gulp.watch(path.src.styles, ['styles']);
+  gulp.watch(path.src.fonts, ['fonts']);
 })
 
-gulp.task('build', ['babel', 'minify-css']);
+
+gulp.task('build', ['scripts', 'styles', 'fonts']);
--- src/scripts/selectshare.js
+++ src/scripts/selectshare.js
@@ -3,23 +3,27 @@ class SelectShare {
     let tooltip = document.createElement('div');
     let socials = ['facebook', 'twitter', 'google'];
 
-    socials.map( social => {
+    socials.map(social => {
       let element = document.createElement('a');
 
-      element.classList.add(`selectshare-${social}`); 
+      element.classList.add(`selectshare-${social}`);
       tooltip.appendChild(element);
     });
 
-    tooltip.classList.add('select-share');
+    tooltip.classList.add('selectshare');
     document.body.appendChild(tooltip);
   }
 
-  getTooltipPosition (textHasSelectedX, textHasSelectedY, selectShareElement) {
+  static getTooltipPosition (textHasSelectedX, textHasSelectedY, selectShareElement) {
     let selectShareStyle = selectShareElement.style;
 
     selectShareStyle.position = 'absolute';
-    selectShareStyle.left = `${textHasSelectedX}px`;
-    selectShareStyle.top = `${textHasSelectedY}px`;
+    selectShareStyle.left = `${textHasSelectedX - 50}px`;
+    selectShareStyle.top = `${textHasSelectedY - 70}px`;
+  }
+
+  appendSocialAddress () {
+
   }
 
   static getSelectedText () {
@@ -29,22 +33,22 @@ class SelectShare {
   static getCurrentPosition () {
     this.createTooltip();
 
-    let selectShareElement = document.querySelector('.select-share');
-    let selectShareText = this.getSelectedText.length;
+    let selectShareElement = document.querySelector('.selectshare');
+    let selectShareText = this.getSelectedText().length;
 
     document.addEventListener('click', function (textHasSelected) {
       let textHasSelectedX = textHasSelected.clientX;
       let textHasSelectedY = textHasSelected.clientY;
       let selectShareClass = selectShareElement.classList;
 
-      new SelectShare().getTooltipPosition(textHasSelectedX, textHasSelectedY, selectShareElement);
+      SelectShare.getTooltipPosition(textHasSelectedX, textHasSelectedY, selectShareElement);
 
       if (selectShareText) {
-        return selectShareClass.remove('select-share-open');
-      } 
+        return selectShareClass.remove('selectshare-open');
+      }
 
-      return selectShareClass.toggle('select-share-open');
-    })
+      return selectShareClass.toggle('selectshare-open');
+    });
   }
 }
 
